4166cf7ee0ecd7096699534ce0da966dff32b086
Matthew <unicomatthew@gmail.com>
2024-05-19 11:40:57 -0700

    created parallax folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import './app.scss'
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
+import Parallax from './components/parallax/Parallax';
 const App = () => {
 
   return (<div>
 
     <section>
       <Navbar id="Homepage" />
-      <Hero/>
+      <Hero />
     </section>
-    <section id='Services'>Parallax</section>
+    <section id='Services'><Parallax /></section>
     <section>Services</section>
     <section id='Portfolio'>Parallax</section>
     <section>Portfolio1</section>
---
+++ src/components/parallax/Parallax.jsx
@@ -0,0 +1,14 @@
+import "./parallax.scss"
+
+const Parallax = ()=>{
+
+    return(
+        <div className="parallax">
+
+
+        </div>
+    )
+}
+
+
+export default Parallax
