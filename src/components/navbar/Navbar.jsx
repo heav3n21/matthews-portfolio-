e1876e09e9b1e7f29180fc4fc7c5811bf57af2d9
Matthew <unicomatthew@gmail.com>
2024-05-18 22:15:14 -0700

    finished slider button on nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import Sidebar from "../sidebar/Sidebar"
 import "./navbar.scss"
-import { animate, motion } from "framer-motion"
+import { motion } from "framer-motion"
 const Navbar = () => {
 
 
     return (
         <div className="navbar">
             <Sidebar />
-            {/* side bar */}
+            side bar
             <div className="wrapper">
                 <motion.span initial={{ opacity: 0, scale: 0.5 }}
                     animate={{ opacity: 1, scale: 1 }}
--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,18 +1,51 @@
+import { motion } from "framer-motion"
+const variants = {
+
+    open: {
+        transition: {
+            staggerChildern: 0.1,
+        },
+    },
+    closed: {
+        transition: {
+            staggerChildern: 0.05,
+            staggerDirection: -1,
+        },
+    },
+};
+const itemsVariants = {
+
+    open: {
+     y:0,
+     opacity:1
+    },
+    closed: {
+      y:50,
+      opacity:0
+    },
+};
 
 
 
+const Links = () => {
 
-const Links = ()=>{
+    const items = [
+        "HomePage",
+        "Services",
+        "Portfolio",
+        "Contact",
+        "About"
 
-    const items=[
-        
     ]
 
-    return (
-        <div  className="links">
 
-Links
-        </div>
+    return (
+        <motion.div className="links" variants={variants}>
+
+            {items.map(item => (
+                <motion.a  href={`#${item}`} key={item} variants={itemsVariants} whileHover={{scale:1.1, }} whileTap={{scale:0.95}}>{item}</motion.a >
+            ))}
+        </motion.div>
     )
 }
 
