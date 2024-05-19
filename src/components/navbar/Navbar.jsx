bf82aa97affc13ce834616d28a73a7415074be69
Matthew <unicomatthew@gmail.com>
2024-05-18 23:28:49 -0700

    created hero page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,7 +7,6 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <Sidebar />
-            side bar
             <div className="wrapper">
                 <motion.span initial={{ opacity: 0, scale: 0.5 }}
                     animate={{ opacity: 1, scale: 1 }}
